73688e324f0b72cbbcb6c1bd13b246a56ab2242e
StanislauWork <1ven970@gmil.com>
2021-08-04 10:58:17 +0300

    fix touchevent

--- main.js
+++ main.js
@@ -32,11 +32,13 @@ class RotatingProduct {
         this.isRotate = false; //возможность вращения
         this.i = 0 // номер каринки
         this.event = {
-            begin: 'mousedown',
-            run: 'mousemove',
-            end: 'mouseup',
-            over: 'mouseenter',
-            leave: 'mouseleave',
+            start: 'click', // тригер для удаления заглушки и начала работы с канвасом
+            begin: 'mousedown', // тригер для beginX
+            run: 'mousemove', // тригер для runX
+            end: 'mouseup', // тригер для endI и удаления тригера run
+            over: 'mouseenter', //тригер для фокуса канваса
+            leave: 'mouseleave', //тригер для удаления фокуса канваса
+            wheel: 'wheel', //тригер для вращения колесом
             beginX: 0, // значение х при событии mousedown
             runX: 0, // значение х при событии mousemove
             endI: 0, // значение this при событии mouseup
@@ -47,19 +49,21 @@ class RotatingProduct {
         this.srcData[0].addEventListener('load', () => this.draw(this.srcData[0]));
         // если устройство телефон - меня события на телефонные
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-            this.event.begin = 'touchstart';
-            this.event.run = 'touchmove';
-            this.event.end = 'touchend';
-        };
-        this.shadow.addEventListener('click', () => {
-            this.shadow.children[0].classList.add('rotate-active');
-            this.allMutation();
-            if (this.isRotate) {
+            this.event.start = 'touchend'; // тригер для удаления заглушки и начала работы с канвасом
+            this.event.begin = 'touchstart'; // тригер для beginX
+            this.event.run = 'touchmove'; // тригер для runX
+            this.event.end = 'touchend'; // тригер для endI и удаления тригера run
+        }
+        // событие начала
+        this.shadow.addEventListener(start, () => {
+            this.shadow.children[0].classList.add('rotate-active'); // запускаю анимацию пока работает allMuttion
+            this.allMutation(); // преобразую строки в картинки
+            if (this.isRotate) { // проверка на возможность вращения
                 // искуственная задержка
                 setTimeout(() => {
-                    this.shadow.children[0].remove('rotate-active');
-                    this.shadow.style.height = 0
-                }, 500)
+                    this.shadow.children[0].remove('rotate-active'); // останавливаю анимацию
+                    this.shadow.style.height = 0; // удаляю перекрывющий канвас элемент  -- переделать на класс
+                }, 500);
             }
         });
         // драг события
@@ -76,7 +80,7 @@ class RotatingProduct {
         // почва для вращения колесом
         this.cvs.addEventListener(this.event.over, () => {
             this.cvs.classList.add('cvs-active');
-            this.cvs.addEventListener('wheel', this.imageReplacementWheel)
+            this.cvs.addEventListener(wheel, this.imageReplacementWheel)
             this.cvs.addEventListener(this.event.leave, () => {
                 this.cvs.classList.remove('cvs-active');
             });
@@ -109,7 +113,7 @@ class RotatingProduct {
                 this.isRotate = true; //разрешаю вращение
                 console.log(this.isRotate)
             }
-        })
+        });
     };
     // метод вращения при помощи перетягивания
     imageReplacementDrag = (e) => {
@@ -155,9 +159,11 @@ let test = new RotatingProduct('cvs', srcData);
 
 /*
     1.Добавить endX чтобы не сбивались координаты - Complete
-    2. разобраться с Sibling
+    2. Разобраться с Sibling
     3. Разобраться с анимацией
-    4. добавить событие колесика, если мышь находиться на канвасе - Complete
-    5. убрать интервал в событии - Complete
-    6. добавить второй cvs-active с теми же стилями но зависящий от драг событий
+    4. Добавить событие колесика, если мышь находиться на канвасе - Complete
+    5. Убрать интервал в событии - Complete
+    6. Добавить второй cvs-active с теми же стилями но зависящий от драг событий
+    7. Переработать пропадание ырфвщц на канвасе
+    8. добавлять соседний элемент чепез конструктор
 */
